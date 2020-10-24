e17e9851c17245409f7bc91e93ea5ddea6e8635f
Justin Perdok <justin-p@users.noreply.github.com>
2020-10-24 16:42:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+REPO_NAME="ansible-playbook-my-linux-workstation"
+REPO_URL="https://github.com/justin-p/$REPO_NAME"
+FOLDER_NAME=$(tr '-' '_' <<<"$REPO_NAME")
+FOLDER_PATH="/tmp/$FOLDER_NAME"
+APT_DEPENDENCIES="git curl python3 python3-pip"
+PIP_DEPENDENCIES="ansible"
+
 sudo echo ''
 echo 'PREPARE [Installing Ansible.] *********************************'
 echo ''
 sudo apt-get update -y >/dev/null 2>&1
-sudo apt-get install git curl python3 python3-pip -y >/dev/null 2>&1
-pip3 install ansible > /dev/null 2>&1
-git clone https://github.com/justin-p/ansible-playbook-my-linux-workstation /tmp/ansible_playbook_my_linux_workstation >/dev/null 2>&1
+sudo apt-get install $APT_DEPENDENCIES -y >/dev/null 2>&1
+pip3 install $PIP_DEPENDENCIES > /dev/null 2>&1
+git clone $REPO_URL $FOLDER_PATH >/dev/null 2>&1
 export PATH=$PATH:/$HOME/.local/bin
 
 echo 'PREPARE [Installing requirements with Ansible-Galaxy.] ********'
 echo ''
-ansible-galaxy install -r /tmp/ansible_my_linux_workstation/requirements.yml >/dev/null 2>&1
+ansible-galaxy install -r $FOLDER_PATH/requirements.yml >/dev/null 2>&1
 echo 'RUN [Running playbook.] ***************************************'
-ansible-playbook /tmp/ansible_playbook_my_linux_workstation/main.yml -i /tmp/ansible_playbook_my_linux_workstation/inventory.yml
+ansible-playbook $FOLDER_PATH/main.yml -i $FOLDER_PATH/inventory.yml
 
 echo -e '\e[35m[WARNING]: Please reboot the system to apply all changes.'
 echo ''
